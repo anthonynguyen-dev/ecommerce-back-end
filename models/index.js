b86cce9b899e96d69fe633d52af4858d4107c546
anthonynguyen-dev <nguyen.dev@protonmail.com>
2023-01-20 16:37:04 -0600

    established relationships between models

--- models/index.js
+++ models/index.js
@@ -1,17 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_tag",
+  onDelete: "CASCADE",
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: ProductTag,
+});
 module.exports = {
   Product,
   Category,
